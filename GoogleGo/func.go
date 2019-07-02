6b9ca14de60f899cfdb0ccf51b79948b1ae45ee6
karthas <zhangqianjuns@163.com>
2019-07-02 16:39:49 +0800

    Google工程师讲解Go：函数参数：值传递

--- GoogleGo/func.go
+++ GoogleGo/func.go
@@ -43,6 +43,15 @@ func sum(numbers ...int) (res int) {
 	return res
 }
 
+func swap1(a, b *int) {
+	*a, *b = *b, *a
+}
+
+// 更合理的方式
+func swap2(a, b int) (int, int) {
+	return b, a
+}
+
 func main() {
 	res, err := eval(3, 4, "x")
 	if err == nil {
@@ -58,4 +67,9 @@ func main() {
 	}, 3, 4))
 
 	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	a, b := 3, 4
+	//swap1(&a, &b)
+	a, b = swap2(a, b)
+	fmt.Println(a, b)
 }
